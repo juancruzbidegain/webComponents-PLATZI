aa2a644ffcc4d046a75bace0f0a57148d0b9ebb5
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-02-14 11:54:07 -0300

    custom-element

--- CustomElements/my-element.js
+++ CustomElements/my-element.js
@@ -1,6 +1,25 @@
+const template = document.createElement("div");
+template.innerHTML = `
+    <style>
+    p{
+        color:blue;
+    }
+    </style>
+    <p class="texto">Hola mundo 2!</p>
+    <strong>Texto ejemplo!</strong>
+`
+
 class myElement extends HTMLElement{
     constructor(){
         super();
-        console.log("Hola mundo!")
+        
+        this.p = document.createElement('p')
+    }
+    connectedCallback(){
+        this.p.textContent = "Hola mundo!"
+        this.appendChild(this.p)
+        this.appendChild(template)
     }
 }
+
+customElements.define('my-element', myElement)
